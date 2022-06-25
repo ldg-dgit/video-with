2214ac415ac037e5054be20bec2cc2d9307f1cea
Bobby Lee <ldg@dgit.co>
2022-06-25 16:46:44 +0900

    Finish initial templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,31 @@
-const user = {
-  username: "Bobby Lee",
-  signin: true,
-};
-
 export const trendingVideos = (req, res) => {
-  return res.render("home", { bodyTitle: "Home", user });
+  const videos = [
+    {
+      title: "First Video",
+      rating: 5,
+      comments: 13,
+      createdAt: "2 minutes ago",
+      views: 5000,
+      id: 1,
+    },
+    {
+      title: "Second Video",
+      rating: 3,
+      comments: 3,
+      createdAt: "1 minutes ago",
+      views: 3000,
+      id: 2,
+    },
+    {
+      title: "Third Video",
+      rating: 4.5,
+      comments: 17,
+      createdAt: "1 second ago",
+      views: 12000,
+      id: 3,
+    },
+  ];
+  return res.render("home", { bodyTitle: "Home", videos, headTitle: "Home" });
 };
 export const videoWatch = (req, res) => {
   return res.render("video_watch", { bodyTitle: "Watch" });
