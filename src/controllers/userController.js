4586dc2822a8de036af2285725b788c6c3fd4177
Bobby Lee <ldg@dgit.co>
2022-06-30 20:45:33 +0900

    Initial Sign In

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import bcrypt from "bcrypt";
 
 export const signupGet = (req, res) => res.render("signup", { bodyTitle: "Sign Up", headTitle: "Sign Up" });
 export const signupPost = async (req, res) => {
@@ -13,18 +14,41 @@ export const signupPost = async (req, res) => {
   if (dupcheck) {
     return res.status(400).render("signup", { bodyTitle, headTitle, errorMessage: "This username/email is already taken." });
   }
-  await User.create({
-    name,
-    username,
-    email,
-    password,
-    location,
-  });
-  return res.redirect("/signin");
+  try {
+    await User.create({
+      name,
+      username,
+      email,
+      password,
+      location,
+    });
+    return res.redirect("/signin");
+  } catch (error) {
+    return res.status(400).render("signup", { headTitle, bodyTitle, errorMessage: error._message });
+  }
+};
+export const signinGet = (req, res) => {
+  const headTitle = "Sign In";
+  const bodyTitle = "Sign In";
+
+  res.render("signin", { bodyTitle, headTitle });
+};
+export const signinPost = async (req, res) => {
+  const headTitle = "Sign In";
+  const bodyTitle = "Sign In";
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(400).render("signin", { bodyTitle, headTitle, errorMessage: "An account with this email does not exists." });
+  }
+  const exists = await bcrypt.compare(password, user.password);
+  if (!exists) {
+    return res.status(400).render("signin", { bodyTitle, headTitle, errorMessage: "Wrong password." });
+  }
+  res.redirect("/");
 };
 export const userEdit = (req, res) => res.send("Edit User");
 export const userDelete = (req, res) => res.send("Delete User");
-export const signin = (req, res) => res.render("signin");
 export const signout = (req, res) => res.send("Sign Out");
 export const userProfile = (req, res) => {
   return res.send(`User Profile @${req.params.id}`);
--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,13 +1,13 @@
 import express from "express";
 
 import { home, search } from "../controllers/videoController";
-import { signupGet, signupPost, signin } from "../controllers/userController";
+import { signupGet, signupPost, signinGet, signinPost } from "../controllers/userController";
 
 const rootRouter = express.Router();
 
 rootRouter.get("/", home);
 rootRouter.route("/signup").get(signupGet).post(signupPost);
-rootRouter.get("/signin", signin);
+rootRouter.route("/signin").get(signinGet).post(signinPost);
 rootRouter.get("/search", search);
 
 export default rootRouter;
