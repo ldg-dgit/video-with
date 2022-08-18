3be1a7e33f3bff77e41ae40d4e7b3d4082a77ca9
Bobby Lee <ldg@dgit.co>
2022-08-19 01:19:44 +0900

    add Realtime comment effect

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -2,6 +2,19 @@ const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 const btn = form.querySelector("button");
 
+const addComment = (text) => {
+  const videoComments = document.querySelector(".video__comments ul");
+  const newComment = document.createElement("li");
+  newComment.className = "video__comment";
+  const icon = document.createElement("i");
+  icon.className = "fas fa-comment";
+  const span = document.createElement("span");
+  span.innerText = ` ${text}`;
+  newComment.appendChild(icon);
+  newComment.appendChild(span);
+  videoComments.prepend(newComment);
+};
+
 const handleSubmit = async (event) => {
   event.preventDefault();
   const textarea = form.querySelector("textarea");
@@ -10,7 +23,7 @@ const handleSubmit = async (event) => {
   if (text === "") {
     return;
   }
-  await fetch(`/api/video/${videoId}/comment`, {
+  const { status } = await fetch(`/api/video/${videoId}/comment`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -18,7 +31,9 @@ const handleSubmit = async (event) => {
     body: JSON.stringify({ text }),
   });
   textarea.value = "";
-  //window.location.reload();
+  if (status === 201) {
+    addComment(text);
+  }
 };
 
 if (form) {
