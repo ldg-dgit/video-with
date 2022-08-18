68f03ed14e7173f3589620fb287c5e798b5c0023
Bobby Lee <ldg@dgit.co>
2022-08-18 22:05:39 +0900

    add comment api

--- src/init.js
+++ src/init.js
@@ -5,7 +5,7 @@ import "./models/Video.js";
 import "./models/Comment.js";
 import app from "./server.js";
 
-const PORT = 3000;
+const PORT = 4000;
 
 const handleListening = () => console.log(`✅ Server listenting on http://localhost:${PORT} 🚀`);
 
--- src/routers/apiRouter.js
+++ src/routers/apiRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
 
-import { viewCount } from "../controllers/videoController.js";
+import { viewCount, createComment } from "../controllers/videoController.js";
 
 const apiRouter = express.Router();
 
 apiRouter.post("/video/:id[0-9a-f]{24}/view/end", viewCount);
+apiRouter.post("/video/:id[0-9a-f]{24}/comment", createComment);
 
 export default apiRouter;
