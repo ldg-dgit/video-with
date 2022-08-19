220cd3e7dec477c72d9e40b3e389d77e08407b34
Bobby Lee <ldg@dgit.co>
2022-08-19 11:11:20 +0900

    PORT env variable

--- src/init.js
+++ src/init.js
@@ -6,7 +6,7 @@ import "./models/Video.js";
 import "./models/Comment.js";
 import app from "./server.js";
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 const handleListening = () => console.log(`✅ Server listenting on http://localhost:${PORT} 🚀`);
 
